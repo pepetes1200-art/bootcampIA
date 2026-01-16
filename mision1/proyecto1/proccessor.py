4b527eb993a290fc4c2e8da830bc9b0c48a65555
Manuel <zapatamanuel.1998@gmail.com>
2026-01-16 11:54:04 -0500

    avances  de requerimiento

--- mision1/proyecto1/proccessor.py
+++ mision1/proyecto1/proccessor.py
@@ -1,4 +1,5 @@
 import re
+from openpyxl import load_workbook
 #========================================
 # funcion celan_id
 #  PUNTO elimina caracteres no numericos de un documento
@@ -19,7 +20,18 @@ def merge_name (name, lastname):
     if lastname is None:
         lastname=""
     return f"{name} {lastname}".strip()
+
+def process_excel(path):
+    # Acceso ala hoja de llamada"datos"
+    wb= load_workbook(path)
+    ws = wb["Datos"]
+    # recorrer todas las filas desde la fila 2 
+    for row in range (2,ws.max_row+1):
+        #columnas D: identificador limpio
+        ws[f"D{row}"] =clean_id(ws[f"A{row}"].value)
+        # columna E: nombre completo 
+        ws[f"E{row}"]=merge_name(
+        ws[f"B{row}"].value,
+        ws[f"C{row}"].value
+        )
         
-        
-            
-            
