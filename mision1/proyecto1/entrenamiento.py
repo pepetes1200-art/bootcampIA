7b129985ab5e260b0ddc97ab901c3251a7459cad
Manuel <zapatamanuel.1998@gmail.com>
2026-01-16 08:31:58 -0500

    nuevo avance

--- mision1/proyecto1/entrenamiento.py
+++ mision1/proyecto1/entrenamiento.py
@@ -1,16 +1,29 @@
-# liberarias
+# librerias
 import re
 
 """
-expresiones  regulares en python
+expresiones regulares en python
 problemas reales 
 """
 
-# codigos 
+#codigo
 print("libreria cargada correctamente")
-# ejemplo
-texto="mi numero es 12345"
-resultado=re.search(r"\d+",texto)
-print(result.group())
+
+#ejemplo 1 
+texto="mi numero es 132"
+resultado= re.search(r"\d+",texto)
+print(f"{texto} Resultado {resultado.group()}")
 texto="mi numero es 12345-985"
-resueltado=re.search(r"\d+",texto)
+resultado=re.search(r"\d+",texto)
+print(f"{texto}resultado{resultado.group()}")
+texto="mi numero es 12345-985"
+resultado=re.findall(r"\d+",texto)
+print(f"{texto} resultado {resultado}")
+
+
+
+documento= "cc.75.055.60"
+
+def clean_id(documento):
+    return re.sub(r"\D","",documento)
+print(clean_id(documento))
